cc4179e4d4226dda316b52854ef6793accfa826f
markoke <markokello55@gmail.com>
2023-12-10 17:12:19 -0500

    readme

--- loadtest/locustfile.py
+++ loadtest/locustfile.py
@@ -1,7 +1,7 @@
 from locust import HttpUser, task, constant
 
 class WebsiteUser(HttpUser):
-    host = "http://20.121.88.29:80" # set host here
+    host = "http://20.121.88.29:80"
     wait_time = constant(0)
 
     @task
